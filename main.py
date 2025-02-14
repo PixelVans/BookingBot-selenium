c5b9b56d15c61a90f32f0b8d12cf203500ce0694
PixelVans <evansmemba001@gmail.com>
2025-02-15 01:15:50 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+os.environ['PATH'] += r"C:\SeleniumDrivers"
+driver = webdriver.Chrome()
+driver.get("https://www.seleniumeasy.com/test/jquery-download-progress-bar-demo.html")
+driver.implicitly_wait(30)
+my_element = driver.find_element(By.ID, "getButtonBoxLink")
+
+my_element.click()
---
+++ run.py
@@ -0,0 +1,13 @@
+from booking.booking import Booking
+
+
+with Booking() as bot:
+    bot.land_first_page()
+    bot.change_currency(currency='USD')
+    bot.select_place_to_go('New York')
+    bot.select_dates(check_in_date='2025-02-22',
+                     check_out_date='2025-02-26')
+    bot.select_adults(1)
+    bot.click_search()
+    bot.apply_filtrations()
+  
