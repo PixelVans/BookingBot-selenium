a3b4f1cf4f6bd834d7543a3605d0e5f409f8c7f6
PixelVans <evansmemba001@gmail.com>
2025-02-16 01:11:25 +0300

    dynamic inputs update

--- run.py
+++ run.py
@@ -1,16 +1,31 @@
 from booking.booking import Booking
 
-
-with Booking() as bot:
-    bot.land_first_page()
-    bot.close_modal_if_present()
-    bot.change_currency(currency='USD')
-    bot.close_modal_if_present()
-    bot.select_place_to_go('New York')
-    bot.select_dates(check_in_date='2025-02-22',
-                     check_out_date='2025-02-26')
-    bot.select_adults(4)
-    bot.click_search()
-    bot.close_modal_if_present()
-    bot.apply_filtrations()
+try
+    with Booking() as bot:
+        bot.land_first_page()
+        bot.close_modal_if_present()
+        bot.change_currency(currency='USD')
+        bot.close_modal_if_present()
+        bot.select_place_to_go(input("Enter your destination"))
+        bot.select_dates(check_in_date=input("Enter your check in date in this format: YYY-MM-DD >"),
+                         check_out_date=input("Enter your check out date in this format: YYY-MM-DD >"))
+        bot.select_adults(input("Enter the number of people >"))
+        bot.click_search()
+        bot.close_modal_if_present()
+        bot.apply_filtrations()
+        
+except Exception as e:
+    if 'in PATH' in str(e):
+        print(
+            'You are trying to run the bot from command line \n'
+            'Please add to PATH your Selenium Drivers \n'
+            'Windows: \n'
+            '    set PATH=%PATH%;C:path-to-your-folder \n \n'
+            'Linux: \n'
+            '    PATH=$PATH:/path/toyour/folder/ \n'
+        )
+    else:
+        raise
   
+
+
